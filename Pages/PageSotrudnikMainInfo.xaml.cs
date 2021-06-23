7ae0821172f277930bf10e1c8399bd470e7ceaed
Skamky <moskam@tuta.io>
2021-06-23 16:21:06 +0500

    Create Page MainSotrudnikInfo

---
+++ Pages/PageSotrudnikMainInfo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_КурсоваяРабота2021_BDZarplata.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageSotrudnikMainInfo.xaml
+    /// </summary>
+    public partial class PageSotrudnikMainInfo : Page
+    {
+        public PageSotrudnikMainInfo()
+        {
+            InitializeComponent();
+            Classes.DB.queryScalar("SELECT  full_name , t2.title, family_status, num_zd_kids,num_invalid_kids, opeka, SpecStatus, StajFROM[bd_zarplta].[sotrudnik] t1 LEFT JOIN bd_zarplta.doljnost t2 ON t1.idDoljnost = t2.idDoljnost");
+        }
+    }
+}
---
+++ Pages/Page_BughalterInfo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_КурсоваяРабота2021_BDZarplata.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page_BughalterInfo.xaml
+    /// </summary>
+    public partial class Page_BughalterInfo : Page
+    {
+        public Page_BughalterInfo()
+        {
+            InitializeComponent();
+            Classes.DB.queryScalar("SELECT      [title]     ,[Oklad], [Travmat] FROM [BD_Zarplata].[bd_zarplta].[doljnost]");
+        }
+    }
+}
